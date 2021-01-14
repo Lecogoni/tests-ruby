111a15ca9233c6127c135cac503a1b39f4a1273a
Lecogoni <giraud.nicolas@me.com>
2021-01-14 10:49:53 +0100

    ficher hello.rb not working

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,7 +1,16 @@
 def hello
-
+  return "Hello!"
 end
 
 def greet(name)
-
+  puts "entre un prénom"
+  print "> "
+  name = gets.chomp.to_s
+  return name
+  puts "Hello, #{name}!"
 end
+
+
+# def greet(Bob)
+#   return "Hello, Bob!"
+# end
